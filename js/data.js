4c911a1dd094c17bad09b4507070e0bb8d293403
kimaiannn07 <alisherovaaiana14@gmail.com>
2024-07-03 13:03:32 +0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,34 @@
+const menu_items = [
+    {
+        id: 1,
+        name: 'miss-dior',
+        price:15  ,
+     img: './js/img/miss-dior.jpeg'
+
+    },
+    {
+        id: 2,
+        name: 'dior',
+        price: 10,
+     img: './js/img/dior.jpeg'
+
+    },
+    {
+        id: 3,
+        name: 'bag',
+        price:11 ,
+     img: './js/img/bag.jpeg'
+
+    },
+    {
+        id:4 ,
+        name: 'diorshow',
+        price:9 ,
+     img: './js/img/diorshow.jpeg'
+
+    },
+    
+  
+]
+
+const orders_basked = []
